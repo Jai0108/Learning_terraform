967a3064abcc662913ce9ef8ff2cfd2af6e8976a
jai0108 <jai.austin01@gmail.com>
2022-07-23 19:11:05 +0530

    version

--- azure/09-resource-syntax-and-behaviour/c1-versions.tf
+++ azure/09-resource-syntax-and-behaviour/c1-versions.tf
@@ -1,10 +1,10 @@
 # terraform setting
 terraform {
-  required_version = "~= 1.0"
+  required_version = "~> 1.2.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 2.66"
+      version = "~> 2.64"
     }
   }
 }
